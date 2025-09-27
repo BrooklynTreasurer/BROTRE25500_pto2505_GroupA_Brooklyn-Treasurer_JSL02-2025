d6ca1bcff79e68b15b9007eee060e87f6b6fd7fd
Brooklyn <markmclean2005@gmail.com>
2025-09-27 11:43:53 +0200

    Added function to convert to lowercase

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ let task1Title = prompt("Enter the title for Task 1:");
 let task1Desc = prompt("Enter the description for Task 1:");
 
 //Task 1 Status
-let task1Status = prompt("Enter the status for Task 1 (To Do, Doing, Done):");
+let task1Status = prompt("Enter the status for Task 1 (To Do, Doing, Done):") .toLowerCase();
