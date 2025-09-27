740c68f3c106786f05358f65c7c4f010378a4786
Brooklyn <markmclean2005@gmail.com>
2025-09-27 11:48:49 +0200

    Added Notified of invalid status and correction prompt

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ let task1Desc = prompt("Enter the description for Task 1:");
 
 //Task 1 Status
 let task1Status = prompt("Enter the status for Task 1 (To Do, Doing, Done):") .toLowerCase();
+while(
+    task1Status !== "to do" &&
+    task1Status !== "doing" &&
+    task1Status !== "done"
+){
+    alert("Invalid status. Please enter 'To Do', 'Doing', or 'Done'.");
+    task1Status = prompt("Enter the status for Task 1 (To Do, Doing, Done):").toLowerCase();
+}
+
