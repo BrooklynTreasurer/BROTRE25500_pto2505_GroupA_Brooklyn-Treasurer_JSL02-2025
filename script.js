535f96ee00ca182c96bdf46a4d655fe26d324ac9
Brooklyn <markmclean2005@gmail.com>
2025-09-27 12:31:06 +0200

    fixed format of console log Done task

--- script.js
+++ script.js
@@ -30,12 +30,12 @@ while(
 
 //Check if task1 is Done
 if (task1Status === "done") {
-    console.log(`Task 1 "${task1Title}" is completed.`);
+    console.log("Title:", ${task1Title} , "status:", ${task1Status});
 }
 
 //Check if task2 is Done
 if (task2Status === "done") {
-    console.log(`Task 2 "${task2Title}" is completed.`);
+    console.log("Title:", ${task2Title} , "status:", ${task2Status});
 }
 
 //If neither task is done
